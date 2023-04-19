a0ec6ba267415024f87852fc30eae6dceba4c754
sonnysam <samuelagbenyo067@gmail.com>
2023-04-19 11:39:58 +0000

    components folder created

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Image from './components/Image';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Image.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import tw from 'twrnc';
+
+export default function Image() {
+  return (
+    <View>
+      <Text>Image</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
